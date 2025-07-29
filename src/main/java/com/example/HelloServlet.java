f8eee330e10c8bf1b934b28119d08c7aa1d439e8
duongvanQuang-bit230347 <dvquang1209@gmail.com>
2025-07-29 17:55:17 +0700

    ncct

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        out.println("<h1>Hello, World, I am a servlet , 10.06.2025!</h1>");
+        out.println("<h1>Hello, World, I am a servlet s , 10.06.2025!</h1>");
 	
 
     }
